946d988372f8ae0ea669bd1dd739f927c36d6348
Alireza Dehghanzadeh <86428901+alerezaaa@users.noreply.github.com>
2022-12-12 10:18:57 +0330

    icon

--- site.config.ts
+++ site.config.ts
@@ -26,7 +26,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: 'favicon-16x16.png',
+  defaultPageIcon: 'public/favicon-16x16.png',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
